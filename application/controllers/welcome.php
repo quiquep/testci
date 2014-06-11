bc51f2e84a12a64692a43a170fca5f74067c99ed
quiquep <enriquepeni@gmail.com>
2014-06-11 10:23:52 -0300

    Added test view.

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -21,6 +21,12 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+	
+	public function test()
+	{
+		$this->load->view('test_message');
+	}
+	
 }
 
 /* End of file welcome.php */
