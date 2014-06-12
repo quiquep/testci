6837455271c76b555316a20008ba7736d54b9db5
quiquep <enriquepeni@gmail.com>
2014-06-12 16:59:55 -0300

    Se agrega PK.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
-CREATE TABLE events (
-	title varchar(30),	
-	description varchar(40),
-	datetime DATE
-);
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) DEFAULT NULL,
+  `description` varchar(40) DEFAULT NULL,
+  `datetime` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
