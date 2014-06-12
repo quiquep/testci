86e6c4340f2e7eebf750335f70999c553a9400ee
Yair <yair.lisa@globallogic.com>
2014-06-12 16:28:53 -0300

    Se agrego creacion,salvado y listado de eventos

--- application/controllers/events.php
+++ application/controllers/events.php
@@ -11,5 +11,26 @@ class Events extends CI_Controller {
 		$this->load->view('event_view' , $data);
 	}
 	
+	public function create()
+	{
+		$this->load->helper('form'); 
+		$this->load->view('create_view');
+	}
 	
+	public function save()
+	{
+			
+			$this->load->model('Event');
+
+			$this->Event->insert_entry();
+			
+			echo 'Se guardo el evento';
+	}
+	
+	public function listAll()
+	{
+		 $this->load->model('Event');
+		 $data['events'] =$this->Event->getAll();
+		 $this->load->view('list_view' , $data);
+	}
 }
--- application/models/Event.php
+++ application/models/Event.php
@@ -17,6 +17,22 @@ class Event extends CI_Model {
 		$event->dateTime = "11/06/2014 11:05";
 		return $event;
 	}
+	
+  	function insert_entry()
+    {
+        $this->title   = $this->input->POST('title'); 
+        $this->description = $this ->input->POST('description');
+        $this->dateTime  = $this->input->POST('datetime');
+        $this->load->database();
+        $this->db->insert('event', $this);
+    }
+    
+    function getAll()
+    {
+    	$this->load->database();
+    	$query = $this->db->get('event');
+	    return $query->result('Event');
+    }
 }
 
 ?>
