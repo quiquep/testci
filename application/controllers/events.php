6e847c6957289a81bb58275b39231d2274cbcda2
quiquep <enriquepeni@gmail.com>
2014-06-13 12:24:32 -0300

    Added autoload for 'url' helper and 'Event' model.

--- application/controllers/events.php
+++ application/controllers/events.php
@@ -9,29 +9,24 @@ class Events extends CI_Controller {
 	
 	public function loadDummyEvent()
 	{
-		$this->load->model('Event');
 		$data['event'] = $this->Event->getDummyEvent();
 		$this->load->view('event_view' , $data);
 	}
 	
 	public function create()
 	{
-		$this->load->helper('url');
 		$this->load->helper('form'); 
 		$this->load->view('create_view');
 	}
 	
 	public function save()
 	{	
-		$this->load->model('Event');
 		$this->Event->insert_entry();
 		$this->listAll();
 	}
 	
 	public function listAll()
 	{
-		$this->load->helper('url');
-		$this->load->model('Event');
 		$data['events'] =$this->Event->getAll();
 		$this->load->view('list_view' , $data);
 	}
