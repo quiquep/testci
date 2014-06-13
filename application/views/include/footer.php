7b92bd91acc7ba9575e48e23e8c388f1ab6a6001
quiquep <enriquepeni@gmail.com>
2014-06-13 20:27:39 -0300

    Added Backbone JS dependencies and initial structure.

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,5 +1,7 @@
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="<?php echo base_url('/js/vendor/jquery-min.js'); ?>"></script>
     <script src="<?php echo base_url('/assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
+	<script src="<?php echo base_url('/js/vendor/underscore-min.js'); ?>"></script>
+	<script src="<?php echo base_url('/js/vendor/backbone-min.js'); ?>"></script>
 </body>
 </html>
