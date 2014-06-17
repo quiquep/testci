74882810a3fe14e039d859ae58986d564f45c615
quiquep <enriquepeni@gmail.com>
2014-06-17 16:50:42 -0300

    Travis CI testing.

--- tests/test.php
+++ tests/test.php
@@ -4,6 +4,16 @@
 
 class Test extends PHPUnit_Framework_TestCase
 {
+	public function setUp()
+	{
+		parent::setUp ();
+	}
+	
+	public function tearDown()
+	{
+		// Nothing
+	}
+		
 	public function testSample() {
 		$this->assertEquals(1, 1);
 	}
