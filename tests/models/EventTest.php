7ecec7cf231761dbd55ccd5f8e04423e5f6db176
quiquep <enriquepeni@gmail.com>
2014-06-18 16:15:01 -0300

    Added dummy event test.

---
+++ tests/models/EventTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class EventModelTest extends PHPUnit_Framework_TestCase
+{
+	private $CI;
+
+	public static function setUpBeforeClass()
+	{
+		$CI =& get_instance();
+		$CI->load->model('Event');
+	}
+
+	public function testDummyEvent()
+	{
+		$event= $this->Event->getDummyEvent();
+		$this->assertTrue("test title" == $event->title);
+	}
+}
+
+?>
