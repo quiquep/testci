29f10bd14211d2dd576626910ddc633e073f9c95
Leonardo De Caria <decarialeonardo@gmail.com>
2014-06-18 10:42:12 -0300

    Added styles to carousel news

--- application/views/news/list.php
+++ application/views/news/list.php
@@ -42,43 +42,45 @@
 
           <h2 class="sub-header">News</h2>
 
-          <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <?php foreach ($news as $key=>$value) {
-                if ( $key == 0 ){?>
-                  <li data-target="#myCarousel" data-slide-to="<?php echo $key; ?>" class="active"></li>
-                <?php } else {?>
-                  <li data-target="#myCarousel" data-slide-to="<?php echo $key; ?>"></li>
-                <?php } ?>
-              <?php } ?>  
-            </ol>
+          <div class="carousel-container">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <?php foreach ($news as $key=>$value) {
+                  if ( $key == 0 ){?>
+                    <li data-target="#myCarousel" data-slide-to="<?php echo $key; ?>" class="active"></li>
+                  <?php } else {?>
+                    <li data-target="#myCarousel" data-slide-to="<?php echo $key; ?>"></li>
+                  <?php } ?>
+                <?php } ?>  
+              </ol>
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-              <?php foreach ($news as $key=>$value) {
-                if ( $key == 0 ){?>
-                  <div class="item active">
-                <?php } else {?>
-                  <div class="item">
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner">
+                <?php foreach ($news as $key=>$value) {
+                  if ( $key == 0 ){?>
+                    <div class="item active">
+                  <?php } else {?>
+                    <div class="item">
+                  <?php } ?>
+                    <img src="<?php echo base_url('/assets/images/bg.jpg'); ?>">
+                    <div class="carousel-caption">
+                        <h3><?php echo $value->title; ?></h3>
+                        <p><?php echo $value->body; ?></p>
+                    </div>
+                </div>
                 <?php } ?>
-                  <img src="<?php echo base_url('/assets/images/bg.jpg'); ?>">
-                  <div class="carousel-caption">
-                      <h3><?php echo $value->title; ?></h3>
-                      <p><?php echo $value->body; ?></p>
-                  </div>
               </div>
-              <?php } ?>
-            </div>
 
-            <!-- Controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-              <span class="glyphicon glyphicon-chevron-left"></span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-              <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
+              <!-- Controls -->
+              <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+              </a>
+              <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+              </a>
+            </div>
           </div>
-        </div>
+        </div>  
 
       </div>
     </div>
